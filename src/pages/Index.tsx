b9cd44b5565fd2b8584ac4827d3c2bbdca21cecf
Jenna Pham <jennaphamu@gmail.com>
2026-03-10 22:57:13 -0700

    Improve hero spacing below header

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -9,8 +9,8 @@ const Index = () => {
   return (
     <div className="min-h-screen bg-background">
       <Header />
-      
-      <main className="pt-28 md:pt-32">
+
+      <main className="pt-32 md:pt-36">
         <HeroSection />
         <ProjectsSection />
         <div className="divider mx-6 md:mx-12 lg:mx-20 max-w-5xl lg:mx-auto" />
